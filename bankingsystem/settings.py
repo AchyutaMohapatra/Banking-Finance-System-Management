bc57838ebdf1fd0c6cefaf48df6f51b5278738af
Achyuta Kumar Mohapatra <79587101+AchyutaMohapatra@users.noreply.github.com>
2022-09-06 16:47:29 +0530

    Update settings.py

--- bankingsystem/settings.py
+++ bankingsystem/settings.py
@@ -167,4 +167,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aba.bankofficial@gmail.com'
-EMAIL_HOST_PASSWORD = '7735792068'
+EMAIL_HOST_PASSWORD = '***************'
